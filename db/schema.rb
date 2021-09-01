d334b482e640550daa35013482ec716c3f0e1d7b
Phat <phat.thanh.113@gmail.com>
2021-09-01 10:26:06 +0700

    done chapter 9

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_02_081711) do
 
+ActiveRecord::Schema.define(version: 2021_09_08_025901) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
