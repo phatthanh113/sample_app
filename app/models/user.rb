4a2a3d96a2e71ef7864967c6df1c78c0f8403374
Phat <phat.thanh.113@gmail.com>
2021-09-01 10:26:06 +0700

    done chapter 8

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, format: {with: VALID_EMAIL_REGEX},
                     length: {maximum: Settings.user.email.max_length}
   has_secure_password
+  def self.digest string
+    cost = if ActiveModel::SecurePassword.min_cost
+             BCrypt::Engine::MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
+    BCrypt::Password.create string, cost: cost
+  end
 end
